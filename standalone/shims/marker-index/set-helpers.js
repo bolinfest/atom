9807fe45d0d3a76da221fa3dd6036569370c3c4a
Michael Bolin <mbolin@fb.com>
2016-11-03 16:01:05 -0700

    Fix a typo from previous commit: "modules" should have been "module".

--- standalone/shims/marker-index/set-helpers.js
+++ standalone/shims/marker-index/set-helpers.js
@@ -20,4 +20,4 @@ let addSet = (set, valuesToAdd) => valuesToAdd.forEach(value => set.add(value));
 
 let intersectSet = (set, other) => set.forEach(function(value) { if (!other.has(value)) { return set.delete(value); } });
 
-modules.export = { setEqual, subtractSet, addSet, intersectSet };
+module.export = { setEqual, subtractSet, addSet, intersectSet };
