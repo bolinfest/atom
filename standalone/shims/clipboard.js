247b5bebf766a5bd72bf81f8abb3aed58710c7cc
Christopher Chedeau <vjeux@fb.com>
2016-11-04 13:17:57 -0700

    clipboard support

--- standalone/shims/clipboard.js
+++ standalone/shims/clipboard.js
@@ -3,25 +3,46 @@ function Clipboard() {
   this.signatureForMetadata = null;
 }
 
+var _currentClipboard = '';
+
 Clipboard.prototype = {
   md5: function(text) {
     // TODO: Pure JS implementation of md5.
     return "6d5c9d3b291785b69f79aa5bda210b79cbb8bd94";
   },
 
-  /** Write the given text to the clipboard. */
   write: function(text, metadata) {
-    // Do nothing.
+    _currentClipboard = text;
   },
-
-  /** Read the text from the clipboard. */
   read: function() {
-    return "";
+    return _currentClipboard;
   },
-
   readWithMetadata: function() {
-    return {text: ""};
+    return {text: _currentClipboard};
   },
 };
+
+['cut', 'copy', 'paste'].forEach(function(eventName) {
+  document.addEventListener(eventName, function(e) {
+    var editor = Array.from(atom.textEditors.editors)[0];
+
+    if (eventName === 'paste') {
+      _currentClipboard = e.clipboardData.getData('text/plain');
+      editor.pasteText();
+    }
+
+    if (eventName === 'copy') {
+      editor.copySelectedText();
+      e.clipboardData.setData('text/plain', _currentClipboard);
+    }
+
+    if (eventName === 'cut') {
+      editor.cutSelectedText();
+      e.clipboardData.setData('text/plain', _currentClipboard);
+    }
+
+    e.preventDefault();
+  });
+});
 
 module.exports = Clipboard;
