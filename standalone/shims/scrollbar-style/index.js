40d05b9635630f3e51031fb8bac2dba4e3bf5461
Michael Bolin <mbolin@fb.com>
2016-11-03 22:28:54 -0700

    Provide a reasonable implementation for scrollbar-style module.

--- standalone/shims/scrollbar-style/index.js
+++ standalone/shims/scrollbar-style/index.js
@@ -1,2 +1,19 @@
-// TODO: Add fake implementations, as necessary.
-module.exports = {};
+const {Emitter} = require('event-kit');
+
+const emitter = new Emitter();
+
+module.exports = {
+  getPreferredScrollbarStyle() {
+    // 'overlay' seems more appropriate than 'legacy'.
+    return 'overlay';
+  },
+
+  onDidChangePreferredScrollbarStyle(callback) {
+    return emitter.on('did-change-preferred-scrollbar-style', callback);
+  },
+
+  observePreferredScrollbarStyle(callback) {
+    callback(this.getPreferredScrollbarStyle());
+    return this.onDidChangePreferredScrollbarStyle(callback);
+  },
+};
